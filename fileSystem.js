34dd6ff083356f0f9fd94128f51f94cd83d24355
Ibu <ibumohamed818@gmail.com>
2025-09-12 17:34:23 +0530

    Refactor fileSystem.js for improved readability and structure

--- fileSystem.js
+++ fileSystem.js
@@ -1,26 +1,38 @@
-const fs=require('fs/promises');
+const fs = require("fs/promises");
 
 // const content=(fs.readFileSync('textFile.txt','utf-8')).toString('utf-8');
 // console.log(content);
 
+(async () => {
 
-(async ()=>{
 
-    const commandFileHandler=await fs.open('command.txt','r');
-   const watcher= fs.watch('./command.txt')
-   for await (const event of watcher){
-    if(event.eventType==='change'){
-        const {size}= await commandFileHandler.stat();
-                const buff= Buffer.alloc(size);
-                const offset=0;
-                const length=size;
-                const position=0;
+  const commandFileHandler = await fs.open("command.txt", "r");
 
-    const content=    await commandFileHandler.readFile(buff,offset,length,position);
-    console.log(content.toString('utf-8'));
-        console.log(content);
+  const CREATE_COMMAND="create a file";
+    const RENAME_COMMAND="rename the file";
+  const DELETE_COMMAND="delete t file";
 
+  commandFileHandler.on("change",async()=>{
+  const { size } = await commandFileHandler.stat();
+      const buff = Buffer.alloc(size);
+      const offset = 0;
+      const length = size;
+      const position = 0;
+
+      const content = await commandFileHandler.readFile(
+        buff,
+        offset,
+        length,
+        position
+      );
+      console.log(content.toString("utf-8"));
+      console.log(content);
+  })
+  const watcher = fs.watch("./command.txt");
+
+   for await (const event of watcher) {
+    if (event.eventType === "change") {
+    commandFileHandler.emit("change")
     }
-}
+  }
 })();
-
