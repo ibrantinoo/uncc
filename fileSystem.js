1bc2f6607838d66ff5bee0732f99cffdf1dd3932
Ibu <ibumohamed818@gmail.com>
2025-09-13 13:26:10 +0530

    ./

--- fileSystem.js
+++ fileSystem.js
@@ -4,35 +4,33 @@ const fs = require("fs/promises");
 // console.log(content);
 
 (async () => {
-
-
   const commandFileHandler = await fs.open("command.txt", "r");
 
-  const CREATE_COMMAND="create a file";
-    const RENAME_COMMAND="rename the file";
-  const DELETE_COMMAND="delete t file";
+  const CREATE_COMMAND = "create a file";
+  const RENAME_COMMAND = "rename the file";
+  const DELETE_COMMAND = "delete the file";
 
-  commandFileHandler.on("change",async()=>{
-  const { size } = await commandFileHandler.stat();
-      const buff = Buffer.alloc(size);
-      const offset = 0;
-      const length = size;
-      const position = 0;
+  commandFileHandler.on("change", async () => {
+    const { size } = await commandFileHandler.stat();
+    const buff = Buffer.alloc(size);
+    const offset = 0;
+    const length = size;
+    const position = 0;
 
-      const content = await commandFileHandler.readFile(
-        buff,
-        offset,
-        length,
-        position
-      );
-      console.log(content.toString("utf-8"));
-      console.log(content);
-  })
+    const content = await commandFileHandler.readFile(
+      buff,
+      offset,
+      length,
+      position
+    );
+    console.log(content.toString("utf-8"));
+    console.log(content);
+  });
   const watcher = fs.watch("./command.txt");
 
-   for await (const event of watcher) {
+  for await (const event of watcher) {
     if (event.eventType === "change") {
-    commandFileHandler.emit("change")
+      commandFileHandler.emit("change");
     }
   }
 })();
